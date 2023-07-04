c20a11ec94cac54066192847740806c3ddf8da08
Jesus Antonio Lopez Reyes <jesus.ant.lopez.reyes@gmail.com>
2023-07-04 08:57:41 -0600

    feature: Inicio del proyecto

---
+++ src/main/java/com/jesuslopdeveloper/mailoshop/MailoshopApiApplication.java
@@ -0,0 +1,13 @@
+package com.jesuslopdeveloper.mailoshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailoshopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailoshopApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jesuslopdeveloper/mailoshop/MailoshopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jesuslopdeveloper.mailoshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailoshopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
